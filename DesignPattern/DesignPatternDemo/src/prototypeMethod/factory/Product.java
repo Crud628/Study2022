b9699924b40676cab77d83a7994819a972773902
Crud628 <1109373967@qq.com>
2022-11-13 21:50:24 +0800

    Prototype（多例）模式

---
+++ DesignPattern/DesignPatternDemo/src/prototypeMethod/factory/Product.java
@@ -0,0 +1,13 @@
+package prototypeMethod.factory;
+
+/**
+ * @author Keason
+ * @version 创建时间：2022年11月13日 下午9:14:04
+ * @TODO
+ * 
+ */
+public interface Product extends Cloneable {
+	public abstract void use(String s);
+
+	public abstract Product createClone();
+}
