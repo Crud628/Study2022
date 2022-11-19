7c9cf9d6f2721308673ebb8d8f0d00f8a642334f
Crud628 <1109373967@qq.com>
2022-11-19 21:45:30 +0800

    Strategy（策略）模式

---
+++ DesignPattern/DesignPatternDemo/src/strategyMethod/WinningStrategy.java
@@ -0,0 +1,27 @@
+package strategyMethod;
+
+import java.util.Random;
+
+/**
+ * @author Keason
+ * @version 创建时间：2022年11月19日 下午9:38:30
+ * @TODO
+ * 
+ */
+public class WinningStrategy implements Strategy {
+    private Random random;
+    private boolean won = false;
+    private Hand prevHand;
+    public WinningStrategy(int seed) {
+        random = new Random(seed);
+    }
+    public Hand nextHand() {
+        if (!won) {
+            prevHand = Hand.getHand(random.nextInt(3));
+        }
+        return prevHand;
+    }
+    public void study(boolean win) {
+        won = win;
+    }
+}
