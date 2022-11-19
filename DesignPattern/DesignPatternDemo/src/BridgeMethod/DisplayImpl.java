5e7752f5361936c05665eccb95fa2b87d35b27d3
Crud628 <1109373967@qq.com>
2022-11-19 21:35:21 +0800

    Bridge（桥）模式

---
+++ DesignPattern/DesignPatternDemo/src/BridgeMethod/DisplayImpl.java
@@ -0,0 +1,15 @@
+package BridgeMethod;
+
+/**
+ * @author Keason
+ * @version 创建时间：2022年11月19日 下午9:30:33
+ * @TODO
+ * 
+ */
+public abstract class DisplayImpl {
+	public abstract void rawOpen();
+
+	public abstract void rawPrint();
+
+	public abstract void rawClose();
+}
