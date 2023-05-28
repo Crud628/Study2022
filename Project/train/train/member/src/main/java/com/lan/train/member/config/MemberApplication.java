8655cf5803c2234786eefffc70b9e84878d8acea
Crud628 <1109373967@qq.com>
2023-05-28 14:38:43 +0800

    配置log

--- Project/train/train/member/src/main/java/com/lan/train/member/config/MemberApplication.java
+++ Project/train/train/member/src/main/java/com/lan/train/member/config/MemberApplication.java
@@ -1,16 +1,24 @@
 package com.lan.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 // 修改位置猴，需要重新定义扫描位置
 @ComponentScan("com.lan")
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 
 }
