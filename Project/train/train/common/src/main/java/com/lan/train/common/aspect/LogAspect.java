3490ad85ed4bd7877141a1687ec8768a846bbc53
Crud628 <1109373967@qq.com>
2023-05-30 21:47:17 +0800

    工具类，log配置完毕

--- Project/train/train/common/src/main/java/com/lan/train/common/aspect/LogAspect.java
+++ Project/train/train/common/src/main/java/com/lan/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.lan.train.member.aspect;
+package com.lan.train.common.aspect;
 
 import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -25,7 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class LogAspect {
     public LogAspect() {
-        System.out.println("LogAspect");
+        System.out.println("common LogAspect");
     }
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
